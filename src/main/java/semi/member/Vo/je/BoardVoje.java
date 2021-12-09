5c289e536621ee2b2c9968012f65f96e946d2d94
nm940 <nm940@DESKTOP-KUFEG38>
2021-12-09 20:07:17 +0900

    aa

---
+++ src/main/java/semi/member/Vo/je/BoardVoje.java
@@ -0,0 +1,19 @@
+package semi.member.Vo.je;
+
+import java.util.Date;
+
+public class BoardVoje {
+	private int service_id;
+	private String hlogin_id;
+	private String title;
+	private String content;
+	private String pwd;
+	private int ref;
+	private int lev;
+	private int step;
+	private Date created_day;
+	private Date update_day;
+	
+	public BoardVOje
+
+}
---
+++ src/main/java/semi/member/dao/je/BoardDaoje.java
@@ -0,0 +1,9 @@
+package semi.member.dao.je;
+
+public class BoardDaoje {
+	
+	public int insert(String id,String title,String content,String pwd) {
+		
+	}
+
+}
