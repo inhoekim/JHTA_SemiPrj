d19be5e8d70bb49d407a1e647681d8c1c2b9a1a0
clgkd <clgkd@DESKTOP-KHU7TER>
2021-12-14 14:06:19 +0900

    비밀번호 변경 내용 수정

--- src/main/java/semi/member/controller/hj/ChangePwdController_hj.java
+++ src/main/java/semi/member/controller/hj/ChangePwdController_hj.java
@@ -18,19 +18,20 @@ public class ChangePwdController_hj extends HttpServlet{
 		String jnum=req.getParameter("jnum");
 		MemberDao dao=new MemberDao();
 		MemberVo vo=dao.selectpwd(hlogin_id, jnum);
-		if(vo==null) {
+		if(vo==null) {	//아이디랑 주민번호가 일치하면 changePwd.jsp페이지로 이동
 			req.setAttribute("result", "fail");
 			req.getRequestDispatcher("/hj/result.jsp").forward(req, resp);
 		}else {
 			req.setAttribute("vo", vo);
-			req.getRequestDispatcher("/hj/changePwd.jsp").forward(req, resp);
+			req.getRequestDispatcher("/hj/changePwd.jsp").forward(req, resp); 
 		}
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
+		String hlogin_id=req.getParameter("hlogin_id");
 		String pwd=req.getParameter("pwd");
-		MemberVo vo=new MemberVo();
+		MemberVo vo=new MemberVo(hlogin_id, pwd, null, null, 0, null, null, 0);
 		MemberDao dao=new MemberDao();
 		int n=dao.changepwd(vo);
 		if(n>0) {
